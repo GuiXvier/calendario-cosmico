8908a00a5eac22e634590c791f9103e2ed25d979
Guilherme Xavier <guilherme.contatogg@gmail.com>
2025-09-08 00:29:54 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/calendario-cosmico/',
-  build: {
-    outDir: 'docs', // <-- Mude 'dist' para 'docs'
-    assetsDir: 'assets'
-  }
+  base: '/calendario-cosmico/'
 })
